932be20204784e9580c18d1f8b10d67fded5bbdc
Thiago Lopes <thiago.lopes@eurekalabs.com.br>
2021-11-14 11:40:08 -0300

    adding input movement + colliders

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+  [SerializeField] float steerSpeed = 300f;
+  [SerializeField] float moveSpeed = 10f;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +16,9 @@ public class Driver : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+    float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+    transform.Rotate(0, 0, -steerAmount);
+    transform.Translate(0, moveAmount, 0);
   }
 }
